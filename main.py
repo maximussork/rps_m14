dadaf7305e3745e5d4d44f9da97f659245b2c2b5
maximussork <119592596+maximussork@users.noreply.github.com>
2023-05-04 21:20:25 +0000

    Actually done something with main.py

--- main.py
+++ main.py
@@ -1,12 +1,77 @@
 from tkinter import *
-root = Tk()
-root.geometry("500x500")
-# main_w = Tk()
-# l_name = Label(text="Type your name", height=10, width=20)
-# name = Entry(width=10)
-# b_name = Button()
-# l_name.pack()
-# name.pack()
-# main_w.mainloop()c
+# App main GUI
+# Windows
 
+root = Tk()
+root.withdraw()
+username_window = Toplevel()
+
+
+#Func
+def username_button_func():
+    username_window.withdraw()
+    root.deiconify()
+
+#username
+var_username = StringVar()
+#
+"""
+Hacer que la string sea sustituida
+if 1:
+    var_username.set("Cojones")
+"""
+
+
+#Miau
+#Parametros
+#root
+root.geometry('400x400')
+root.title("Rock Python Scissors ")
+root_label = Label(
+    root,
+    text="Rock Python Scissors",
+    font = ("Opensans 25 bold")
+)
+root_label_username = Label(
+    root,
+    text="Username:",
+    font = ("Opensans 20")
+)
+root_button = Button(
+    root,
+    text = "PLAY",
+    font = ("Opensans 25 bold"),
+)
+root_user_var = Label(
+    root,
+    textvariable = var_username,
+    font = ("Opensans 20")
+)
+#username_window
+username_window.geometry("400x400")
+username_window.title("Introduce your username")
+username_window_label = Label(
+    username_window,
+    text="Introduce your username:"
+)
+username_window_entry = Entry(
+    username_window,
+    textvariable = var_username
+
+)
+username_window_button = Button(
+    username_window,
+    text= "Enter",
+    font = ("Opensans 25 bold"),
+    command = username_button_func
+)
+#Packing
+root_label.pack()
+root_label_username.pack()
+root_user_var.pack()
+root_button.pack()
+username_window_label.pack()
+username_window_entry.pack()
+username_window_button.pack()
+#INICIAR
 root.mainloop()
